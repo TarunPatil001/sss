3cc4b8084e99d39b397b6d62a67dbf2f4a720ba1
Tarun Kishor Patil <tarunpatil001@gmail.com>
2024-12-01 16:05:10 +0530

    error resolved

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ function Footer({}: Props) {
     ["ABOUT", "Newsroom", "Learn about new features", "Letter from our founders", "Careers", "Investors"],
     ["Support", "Help Center", "AirCover", "Cancellation options", "Safety information", "Report a neighborhood concern"],
     ["Community", "Newsroom", "Learn about new features", "Letter from our founders", "Careers", "Investors"],
-    ["Hosting","Try hosting","AirCover for Hosts","Explore hosting resources","Safety information","How to host responsibly"],
+    ["Hosting", "Try hosting", "AirCover for Hosts", "Explore hosting resources", "Safety information", "How to host responsibly"],
   ];
 
   useEffect(() => {
@@ -26,8 +26,8 @@ function Footer({}: Props) {
   }, []);
 
   const footerColumns = itemData.map((item, index) => (
-    <FooterColumn index={index} data={item} />
-  ))
+    <FooterColumn key={index} index={index} data={item} />
+  ));
 
   return (
     <ClientOnly>
